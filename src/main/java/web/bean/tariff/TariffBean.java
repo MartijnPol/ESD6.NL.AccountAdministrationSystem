1c537bc58b951eb107b22958b28841ebf75b0eeb
Violet Kampers <282404@student.fontys.nl>
2018-04-06 12:57:22 +0200

    Adding the create option for tariffs

--- src/main/java/web/bean/tariff/TariffBean.java
+++ src/main/java/web/bean/tariff/TariffBean.java
@@ -2,6 +2,7 @@ package web.bean.tariff;
 
 import main.domain.Tariff;
 import main.service.TariffService;
+import web.core.helper.RedirectHelper;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
@@ -19,12 +20,40 @@ public class TariffBean implements Serializable{
     private List<Tariff> tariffs;
     private Tariff tariff;
 
+    private double tariffInEuro;
+    private boolean ridingDuringRushHour;
+
+
     @PostConstruct
     public void init() {
         this.tariffs = this.tariffService.findAll();
     }
 
+    public void create(){
+        if (tariffInEuro != 0) {
+            Tariff newTariff = new Tariff(this.tariffInEuro, this.ridingDuringRushHour);
+            tariffService.createOrUpdate(newTariff);
+        }
+        RedirectHelper.redirect("/pages/tariff/tariffOverview.xhtml");
+    }
+
     public List<Tariff> getTariffs() { return tariffs; }
 
     public void setTariffs(List<Tariff> tariffList) { this.tariffs = tariffList; }
+
+    public double getTariffInEuro() {
+        return tariffInEuro;
+    }
+
+    public void setTariffInEuro(double tariffInEuro) {
+        this.tariffInEuro = tariffInEuro;
+    }
+
+    public boolean isRidingDuringRushHour(){
+        return this.ridingDuringRushHour;
+    }
+
+    public void setRidingDuringRushHour(boolean ridingDuringRushHour) {
+        this.ridingDuringRushHour = ridingDuringRushHour;
+    }
 }
