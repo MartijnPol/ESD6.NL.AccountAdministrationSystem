d9698f6fe14cc33743c1bfa6755493b05d4f8316
Thom van de Pas <thomvandepas@hotmail.com>
2018-04-23 13:25:46 +0200

    Simple null check

--- src/main/java/web/bean/invoice/InvoiceBean.java
+++ src/main/java/web/bean/invoice/InvoiceBean.java
@@ -7,7 +7,6 @@ import web.bean.BaseBean;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.math.BigDecimal;
 
 /**
  * @author Thom van de Pas on 8-4-2018
@@ -26,7 +25,9 @@ public class InvoiceBean extends BaseBean {
     @Override
     public void init() {
         this.invoice = this.invoiceService.findByInvoiceNr(this.invoiceNr);
-        this.monthName = this.invoiceService.getMonthName(this.invoice.getPeriod());
+        if (this.invoice != null) {
+            this.monthName = this.invoiceService.getMonthName(this.invoice.getPeriod());
+        }
     }
 
     //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
