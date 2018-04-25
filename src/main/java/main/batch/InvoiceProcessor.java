7114b32d182e4473775e77f85dea24bcfb4fbbed
Thom van de Pas <thomvandepas@hotmail.com>
2018-04-25 15:19:01 +0200

    Progress with Batch Processing

--- src/main/java/main/batch/InvoiceProcessor.java
+++ src/main/java/main/batch/InvoiceProcessor.java
@@ -1,8 +1,13 @@
 package main.batch;
 
+import main.domain.Invoice;
+
 import javax.batch.api.chunk.ItemProcessor;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Thom van de Pas on 25-4-2018
@@ -11,8 +16,15 @@ import javax.inject.Named;
 @Named("InvoiceProcessor")
 public class InvoiceProcessor implements ItemProcessor {
 
+    @Inject
+    private Logger logger;
+
     @Override
-    public Object processItem(Object o) throws Exception {
-        return null;
+    public Object processItem(Object object) throws Exception {
+
+        Invoice invoice = (Invoice) object;
+        logger.log(Level.INFO, "Started Batch Process of Invoice with number: " + invoice.getInvoiceNr());
+
+        return invoice;
     }
 }
--- src/main/java/main/batch/InvoiceWriter.java
+++ src/main/java/main/batch/InvoiceWriter.java
@@ -1,10 +1,16 @@
 package main.batch;
 
+import main.domain.Invoice;
+import main.service.InvoiceService;
+
 import javax.batch.api.chunk.ItemWriter;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Thom van de Pas on 25-4-2018
@@ -12,6 +18,11 @@ import java.util.List;
 @Dependent
 @Named("InvoiceWriter")
 public class InvoiceWriter implements ItemWriter {
+
+    @Inject
+    private InvoiceService invoiceService;
+    @Inject
+    private Logger logger;
 
     @Override
     public void open(Serializable serializable) throws Exception {
@@ -24,7 +35,15 @@ public class InvoiceWriter implements ItemWriter {
     }
 
     @Override
-    public void writeItems(List<Object> list) throws Exception {
+    public void writeItems(List<Object> items) throws Exception {
+
+        for (Object item : items) {
+            Invoice invoice = (Invoice) item;
+            logger.log(Level.INFO, "Generating PDF for: " + invoice.getInvoiceNr());
+            //TODO: Generate PDF here.
+            //Here the pdf gets generated.
+//            this.invoiceService.generateInvoicePdf(invoice);
+        }
 
     }
 
