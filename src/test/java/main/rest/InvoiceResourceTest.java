bef529e6127cbe670467a35bb46bb1cb0b7de388
Thom van de Pas <thomvandepas@hotmail.com>
2018-05-23 15:15:32 +0200

    Finished getInvoicesForOwner

---
+++ src/test/java/main/rest/InvoiceResourceTest.java
@@ -0,0 +1,36 @@
+package main.rest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.when;
+import static main.rest.RestTestSetup.setUpRestResource;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+/**
+ * @author Thom van de Pas on 23-5-2018
+ */
+public class InvoiceResourceTest {
+
+    @BeforeClass
+    public static void setUp() {
+        setUpRestResource();
+    }
+
+    @Test public void
+    invoice_resource_returns_200_with_expected_invoiceNr() {
+                when().
+                        get("/invoices/{citizenServiceNumber}", 1234567890).
+                then().
+                        statusCode(200).
+                        body("invoiceNr", hasItems(20181, 20182));
+    }
+
+    @Test public void
+    invoice_resource_returns_404() {
+        when().
+                get("/invoices/{citizenServiceNumber}", 1234567891).
+        then().
+                statusCode(404);
+    }
+}
